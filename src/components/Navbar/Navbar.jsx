4473aca7e8955b3e091a11243a2a55244d4d7a09
1ashutoshverma <ashu9236047613@gmail.com>
2023-10-18 00:41:19 +0530

    created mobile navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from "./NavbarImages/Logo.svg"
 import cart from "./NavbarImages/Cart.svg"
 import search from "./NavbarImages/SearchIcon.svg"
+import burger from "./NavbarImages/BurgerMenu.svg"
 import style from "./Navbar.module.css"
+// import MenuIcon from '@mui/icons-material/Menu';
 const Navbar = () => {
+    const [clickedHumburger, setClickedHumburger] = useState(false)
     return (
         <div id={style.navbar_container}>
+            {/* ----------Navbar for Laptop Screen Starts Here ------------ */}
             <div className={style.navbar_bigscreen}>
                 <div className={style.logo_navbar}><img src={logo} alt="Logo" /></div>
                 <div className={style.navbar_content_container}>
@@ -17,21 +21,43 @@ const Navbar = () => {
                             <p>Blog</p>
                             <p>Portfolio</p>
                             <p>About us</p>
-                            <p>Dubai</p>
-                            <p>Contacts</p>
                         </div>
                         <div>
                             <div className={style.search_icon}>
                                 <input type="text" placeholder='Search Items...' />
                                 <img src={search} alt="" />
                             </div>
-                            <p>Login</p>
-                            <img src={cart} alt="" style={{ width: "25px" }} />
+                            <p><b>Login</b></p>
+                            <img src={cart} className={style.navbar_cart} />
                         </div>
                     </div>
                     <div className={style.navbar_bottom}>
+                        <div>
+                            <p>SOFAS</p>
+                            <p>BED</p>
+                            <p>CHILDREN'S FURNITURE</p>
+                            <p>ARMCHAIRS AND POUFS</p>
+                        </div>
+                        <div>
+                            <div><b>+7 (926) 787-11-00</b></div>
+                            <p>Modern Furniture factory</p>
+                        </div>
 
                     </div >
+                </div>
+            </div>
+            {/* ----------Navbar for Laptop Screen Ends Here ------------ */}
+            <div className={style.navbar_smallscreen}>
+                <div className={style.logo_mobile}><img src={logo} alt="Logo" /></div>
+                <div className={style.navbar_mobile}>
+                    <div className={style.search_icon}>
+                        <input type="text" placeholder='Search Items...' />
+                        <img src={search} alt="" />
+                    </div>
+                    <p><b>Login</b></p>
+                    <img src={cart} className={style.navbar_cart} />
+                    {/* {MenuIcon} */}
+                    <img src={burger} className={style.navbar_humburger} />
                 </div>
             </div>
         </div>
