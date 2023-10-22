aa3b667afee4f777be2adf556e0559339bba1d17
1ashutoshverma <ashu9236047613@gmail.com>
2023-10-23 00:42:10 +0530

    name changed photogallry

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Carousel from '../components/HomePage/Carousel'
 import Section1 from '../components/HomePage/Section1'
 import Section2 from '../components/HomePage/Section2'
-import Photogallary from '../components/HomePage/Photogallary'
+import PhotoGallary from '../components/HomePage/PhotoGallary'
 import Team from '../components/HomePage/Team'
 import ContactsUs from '../components/HomePage/ContactsUs'
 import { useSelector } from 'react-redux'
@@ -18,7 +18,7 @@ const Homepage = () => {
             <BuyersChoice />
             <Section1 />
             <Section2 />
-            <Photogallary />
+            <PhotoGallary />
             <Team />
             <ContactsUs />
             {/* <div style={{ height: "500px", border: "1px solid red" }} ></div> */}
