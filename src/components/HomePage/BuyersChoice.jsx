b91de3ae056753381ab55abcf011a38d1c406c19
1ashutoshverma <ashu9236047613@gmail.com>
2023-10-22 20:20:50 +0530

    done catalog

--- src/components/HomePage/BuyersChoice.jsx
+++ src/components/HomePage/BuyersChoice.jsx
@@ -1,21 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import style from "./BuyersChoice.module.css"
+import axios from 'axios'
 
 const BuyersChoice = () => {
+    const [type, setType] = useState("Sofas");
+    const [data, setData] = useState([]);
+
+    const getData = async (type) => {
+        try {
+            const res = await axios(`https://crudoperations-b7d45-default-rtdb.firebaseio.com/${type}.json`)
+            const gotdata = res.data;
+            console.log(res.data)
+            let arr = [];
+            for (let i = 0; i < gotdata.length; i++) {
+                if (i < 4) {
+                    arr.push(gotdata[i])
+                } else {
+                    break;
+                }
+            }
+            setData(arr);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    useEffect(() => {
+        getData(type)
+    }, [type])
+    console.log(data);
     return (
         <div className={style.BuyersChoice}>
             <div>
                 <h1>Buyers' choice</h1>
                 <div className={style.linkProvider}>
                     <div>
-                        <p>SOFAS</p>
-                        <p>SOFAS</p>
-                        <p>SOFAS</p>
-                        <p>SOFAS</p>
+                        <p id={type == "Sofas" ? style.selectedEffect : ""} onClick={() => { setType("Sofas") }}>SOFAS</p>
+                        <p id={type == "Beds" ? style.selectedEffect : ""} onClick={() => { setType("Beds") }}>BEDS</p>
+                        <p id={type == "ChildrenFurniture" ? style.selectedEffect : ""} onClick={() => { setType("ChildrenFurniture") }}>CHILDREN FURNITURE</p>
+                        <p id={type == "ArmChair" ? style.selectedEffect : ""} onClick={() => { setType("ArmChair") }}>ARMCHAIRS AND POUFS</p>
                     </div>
                     <div>
                         <h2>GO TO CATALOG</h2>
                     </div>
+                </div>
+                <div className={style.cardWrapper}>
+                    {
+                        data.map((e) => {
+                            return (
+                                <div key={e.id}>
+                                    <img src={e.image} alt="" />
+                                    <p> from ₹{e.price} </p>
+                                    <h3>{e.title}</h3>
+                                    <div>
+                                        <h4>{e.type}</h4>
+                                        <button>Buy Now</button>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
         </div>
